02fb960e88d63511324f35d8e038fbd0c84f97ca
Christina Rontell <christina-rontell@pluralsight.com>
2023-07-10 17:02:35 -0500

    req set up

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require("express");
 var app = express();
+
+app.get('/', (rq, res, next)=> {
+    res.send('Hello World')
+})
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+  })
+
 app.listen(4000, () => {
- console.log("Server running on port 4000");
+ console.log("Server running on port 4000: http://localhost:4000/");
 });
